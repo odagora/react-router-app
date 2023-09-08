99f9a3ba0cc6d05a2d5fd6697672d887806ae20e
odagora <oscar.d.gonzalez@gmail.com>
2023-09-08 11:51:49 -0500

    feat: :sparkles: added private and public routes feature

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,44 +1,58 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 const routes = [];
 routes.push({
   to: "/",
   text: "Home",
+  private: false,
 });
 routes.push({
   to: "/blog",
   text: "Blog",
+  private: false,
 });
 routes.push({
   to: "/profile",
   text: "Profile",
+  private: true,
 });
 routes.push({
   to: "/login",
   text: "Login",
+  private: false,
+  publicOnly: true,
 });
 routes.push({
   to: "/logout",
   text: "Logout",
+  private: true,
 });
 
 export const Menu = () => {
+  const auth = useAuth();
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.text}>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "yellow" : "white",
-              })}
-              to={route.to}
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
+          if (route.private && !auth.user) return null;
+
+          if (route.publicOnly && auth.user) return null;
+
+          return (
+            <li key={route.text}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "yellow" : "white",
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
