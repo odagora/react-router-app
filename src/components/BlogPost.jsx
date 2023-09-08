6dd74a460dab6b8e6df4470ad8f3d70a4327f065
odagora <oscar.d.gonzalez@gmail.com>
2023-09-08 11:26:03 -0500

    feat: :sparkles: added main blog page redirect from blog posts

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./BlogPage";
 
 export const BlogPost = () => {
+  const navigate = useNavigate();
   const { slug } = useParams();
-
   const blogpost = blogdata.find((post) => post.slug === slug);
+
+  const returnBlog = () => {
+    navigate("/blog");
+  };
 
   return (
     <>
       <h2>{blogpost.title}</h2>
+
+      <button onClick={returnBlog}>Volver al blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
