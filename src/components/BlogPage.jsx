c3be133b1823594a464e11c12a5f9645526295c9
odagora <oscar.d.gonzalez@gmail.com>
2023-09-08 11:22:31 -0500

    feat: :sparkles: added BlogPost component and dynamic routing

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function BlogPage() {
-  return <h1>Blog Page</h1>;
-}
+export const blogdata = [];
 
-export { BlogPage };
+blogdata.push({
+  title: "¿Que es React?",
+  slug: "que-es-react",
+  content: "React es el mejor Framework de JavaScript, que lindo React",
+  author: "Andrés Rodríguez",
+});
+
+blogdata.push({
+  title: "¿Que es Vue?",
+  slug: "que-es-vue",
+  content: "Vue es el mejor Framework de JavaScript, que lindo Vue",
+  author: "Andrés Rodríguez",
+});
+
+blogdata.push({
+  title: "¿Que es Angular?",
+  slug: "que-es-angular",
+  content: "Angular esta bien, que lindo React XD",
+  author: "Andrés Rodríguez",
+});
+
+blogdata.push({
+  title: "¿Que es Svelte?",
+  slug: "que-es-svelte",
+  content: "Svelte es el mejor Framework de JavaScript, que lindo Svelte",
+  author: "Andrés Rodríguez",
+});
+
+export const BlogPage = () => {
+  return (
+    <>
+      <h1>BlogPage</h1>
+      <ul>
+        {blogdata.map((post) => (
+          <BlogLink key={post.title} post={post} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const BlogLink = ({ post }) => {
+  return (
+    <li>
+      <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+    </li>
+  );
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { useRoutes } from "react-router-dom";
 import { HomePage } from "../components/HomePage";
 import { BlogPage } from "../components/BlogPage";
 import { ProfilePage } from "../components/ProfilePage";
+import { BlogPost } from "../components/BlogPost";
 
 export default function Router() {
   let element = useRoutes([
@@ -12,6 +13,10 @@ export default function Router() {
     {
       path: "/blog",
       element: <BlogPage />,
+    },
+    {
+      path: "/blog/:slug",
+      element: <BlogPost />,
     },
     {
       path: "/profile",
